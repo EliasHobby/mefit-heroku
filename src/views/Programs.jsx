540335c0f7326fb490208bf545089df8c1828c86
Elias <elias.kloverod.brynestad@no.experis.com>
2022-10-04 15:44:45 +0200

    feat: added program-card-component

--- src/views/Programs.jsx
+++ src/views/Programs.jsx
@@ -1,6 +1,11 @@
+import ProgramCard from "../components/ProgramCard/ProgramCard";
+
 const Programs = () => {
     return (
-        <h1>Programs</h1>
+        <>
+            <h1>Programs</h1>
+            <ProgramCard></ProgramCard>
+        </>
     )
 }
 export default Programs;
