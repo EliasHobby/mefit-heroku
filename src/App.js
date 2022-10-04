356cb050b9af044aa404efadec54f4c6f052fdad
Elias <elias.kloverod.brynestad@no.experis.com>
2022-10-04 10:48:57 +0200

    feat: added routing to login and profile

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Login from './views/Login';
+import Profile from './views/Profile';
+
 
 function App() {
+
+
+
   return (
-    <div className="App">
-      
-    </div>
+    <BrowserRouter>
+      <div className='App'>
+        <Routes>
+          <Route path="/" element={ <Login /> } />
+          <Route path="/profile" element={ <Profile /> } />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
