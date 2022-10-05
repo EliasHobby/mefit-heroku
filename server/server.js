900964039b403eb3dccb719a3151099646672ad1
Elias <elias.kloverod.brynestad@no.experis.com>
2022-10-05 10:24:09 +0200

    feat: added an https check in express server to enforce https connection for all traffic

--- server/server.js
+++ server/server.js
@@ -9,3 +9,12 @@ app.get('*', (req, res) => {
 app.listen(port, () => {
    console.log('Server is up!');
 });
+
+if(process.env.NODE_ENV === 'production') {
+    app.use((req, res, next) => {
+      if (req.header('x-forwarded-proto') !== 'https')
+        res.redirect(`https://${req.header('host')}${req.url}`)
+      else
+        next()
+    })
+  }
