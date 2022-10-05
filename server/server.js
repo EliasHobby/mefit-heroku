7dc218ca18ee92acb1a87108fc893e43902e0b2f
Elias <elias.kloverod.brynestad@no.experis.com>
2022-10-05 10:32:26 +0200

    fix: attempting to enforce https connection for all traffic

--- server/server.js
+++ server/server.js
@@ -2,18 +2,22 @@ const path = require('path');
 const express = require('express');
 const app = express();
 const publicPath = path.join(__dirname, '..', 'build');
-const port = process.env.PORT || 3000;app.use(express.static(publicPath));
+const port = process.env.PORT || 3000; app.use(express.static(publicPath));
 
-if(process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === 'production') {
+
     app.use((req, res, next) => {
         if (req.header('x-forwarded-proto') !== 'https')
-        res.redirect(`https://${req.header('host')}${req.url}`)
+            res.redirect(`https://${req.header('host')}${req.url}`)
         else
-        res.sendFile(path.join(publicPath, 'index.html'));
+            res.sendFile(path.join(publicPath, 'index.html'));
         next()
     })
-  }
+}
+app.get('*', (req, res) => {
+
+});
 app.listen(port, () => {
-   console.log('Server is up!');
+    console.log('Server is up!');
 });
 
