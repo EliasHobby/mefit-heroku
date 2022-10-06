19bc607c20337a896a98b040821d48ad3f4f5e3b
Elias <elias.kloverod.brynestad@no.experis.com>
2022-10-06 15:51:51 +0200

    feat: finished setting up keycloak, now need to configure redirection and fix error with missing clientId

--- src/Keycloak.js
+++ src/Keycloak.js
@@ -1,8 +1,13 @@
 import Keycloak from "keycloak-js";
-const keycloak = new Keycloak({
- url: "https://react-mefit.herokuapp.com/",
- realm: "MeFit",
- clientId: "MeFit",
-});
+const keycloak = new Keycloak(
+    {
+        "realm": "mefit",
+        "auth-server-url": "https://mefit-keycloak-api.herokuapp.com/auth/",
+        "ssl-required": "external",
+        "resource": "mefit",
+        "public-client": true,
+        "confidential-port": 0
+    }
+);
 
 export default keycloak
