ae4fff152f43070dbb7d52bbdd35b7e023b3c695
Robin Hextall-Schjelderup <robinh-s@hotmail.com>
2022-10-18 09:56:45 +0200

    Merge branch 'main' into add-Pages(Workout-Program

--- src/views/Programs.jsx
+++ src/views/Programs.jsx
@@ -32,7 +32,6 @@ const Programs = () => {
                   <Grid item xs={4} mb={4} key={index}>
                       <DisplayCard element={program} id={index} type="program" />
                       <Typography variant="h5">Hi {data.type} from {data.country}.</Typography>
-
                   </Grid>
               ))}                  
               
